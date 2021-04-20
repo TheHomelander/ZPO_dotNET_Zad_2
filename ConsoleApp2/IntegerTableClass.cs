73cad5220bdc34b698957127bad8afef1ffc3284
Wiktor Koperek <222200@edu.p.lodz.pl>
2021-04-20 13:29:15 +0200

    first commit

---
+++ ConsoleApp2/IntegerTableClass.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public delegate void Notify(int[] size);
+    class IntegerTableClass
+    {
+        public event Notify ZmieanaRozmiaru;
+        private int[,] twoDimensionIntegerArray;
+        public int[] TwoDimensionIntegerArray
+        {
+            get
+            {
+                int[] temp =  { twoDimensionIntegerArray.GetLength(0), twoDimensionIntegerArray.GetLength(1) } ;
+                return temp;
+            }
+        }
+
+        
+        public int this[int row, int col]
+        {
+            get
+            {
+                try
+                {
+                    
+                    return twoDimensionIntegerArray[row, col];
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    throw new ArgumentException("Index is out of range", nameof(col) + nameof(row), ex);
+                }
+            }
+
+            set
+            {
+                int currentRow = twoDimensionIntegerArray.GetLength(0);
+                int currentColumn = twoDimensionIntegerArray.GetLength(1);
+
+                int[] pasEv = { row + 1, col + 1 };
+                if ( currentColumn - 1 < col && currentRow - 1 < row)
+                {
+                    ZmieanaRozmiaru?.Invoke(pasEv);
+                    int[,] tempArr = new int[row + 1, col + 1];
+                    for (int i = 0; i < currentRow; i++)
+                    {
+                        for(int j = 0; j < currentColumn; j++)
+                        {
+                            tempArr[i, j] = twoDimensionIntegerArray[i, j];
+                        }
+                    }
+                    twoDimensionIntegerArray = tempArr;
+                }else if ( currentColumn - 1 < col)
+                {
+                    ZmieanaRozmiaru?.Invoke(pasEv);
+                    int[,] tempArr = new int[currentRow, col + 1];
+                    for (int i = 0; i < currentRow; i++)
+                    {
+                        for (int j = 0; j < currentColumn; j++)
+                        {
+                            tempArr[i, j] = twoDimensionIntegerArray[i, j];
+                        }
+                    }
+                    twoDimensionIntegerArray = tempArr;
+                }else if (currentRow - 1 < row)
+                {
+                    ZmieanaRozmiaru?.Invoke(pasEv);
+                    int[,] tempArr = new int[row + 1, currentColumn];
+                    for (int i = 0; i < currentRow; i++)
+                    {
+                        for (int j = 0; j < currentColumn; j++)
+                        {
+                            tempArr[i, j] = twoDimensionIntegerArray[i, j];
+                        }
+                    }
+                    twoDimensionIntegerArray = tempArr;
+                }
+                
+                twoDimensionIntegerArray[row, col] = value;
+            }
+        }
+
+        
+        public IntegerTableClass(int rows, int columns)
+        {
+            twoDimensionIntegerArray = new int[rows, columns];
+        }
+
+
+
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegerTableClass newObject = new IntegerTableClass(2, 1);
+            newObject.ZmieanaRozmiaru += registerEvent;
+            newObject[0, 0] = 1;
+            newObject[0, 1] = 1;
+            newObject[1, 0] = 2;
+            newObject[1, 1] = 2;
+            newObject[1, 2] = 3;
+
+            for( int i = 0; i < 2; i++)
+            {
+                for (int j = 0; j < 2; j++) {
+                    Console.WriteLine(newObject[i, j]);
+                }
+            }
+            Console.WriteLine(newObject[1, 2]);
+            int[] temp = newObject.TwoDimensionIntegerArray;
+            Console.WriteLine("ROZMIAR\n");
+            Console.WriteLine(temp[0]);
+            Console.WriteLine(temp[1]);
+        }
+        public static void registerEvent(int[] args)
+        {
+            Console.WriteLine("EVENT START");
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            Console.WriteLine("EVENT END");
+        }
+    }
+}
